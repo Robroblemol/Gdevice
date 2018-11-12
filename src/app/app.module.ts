d75725ae903c729b4779855cc6df6065e6949054
Raziel <robroblemol>
2018-11-11 20:32:17 -0500

    credos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { DevicesComponent } from './components/devices/devices.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
+    DevicesComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
