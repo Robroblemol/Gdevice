67d81494201cf0b98d76466a56e4b1ead7012f6a
Raziel <robroblemol>
2018-11-11 23:25:44 -0500

    adding dependencies for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDOx7nG2IvFIqlDvDHaSoNwJJ21F-l3114",
+    authDomain: "future-da795.firebaseapp.com",
+    databaseURL: "https://future-da795.firebaseio.com",
+    projectId: "future-da795",
+    storageBucket: "future-da795.appspot.com",
+    messagingSenderId: "820242584864"
+  }
 };
