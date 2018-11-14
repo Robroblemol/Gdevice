50796815e080f58540559f32d4c77781c76e9590
Raziel <robroblemol>
2018-11-13 23:22:17 -0500

    adding login and logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  public isLogin: boolean;
+  public userName: string;
+  public userEmail: string;
 
-  constructor() { }
+  constructor(
+    public authService:AuthService
+  ) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe( auth =>  {
+      if(auth){
+        this.isLogin = true;
+        this.userName = auth.displayName;
+        this.userEmail = auth.email;
+      }else{
+        this.isLogin = false;
+      }
+    })
   }
-
+  onClickLogout(){
+    this.authService.logout();
+  }
 }
